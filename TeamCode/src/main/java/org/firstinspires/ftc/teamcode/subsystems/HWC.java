2ec9fd26dca1ae7e57b93f828a0303171508f1fa
A. Teo Welton <76081718+DragonDev07@users.noreply.github.com>
2024-02-24 11:04:29 -0700

    Change claw pos back to what it was before, told you so omar. (#100)

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HWC.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/HWC.java
@@ -41,7 +41,7 @@ public class HWC {
     public static double passoverDeliveryPos = 0.2;
     public static double passoverIntakePos = 0.8;
     public static double wristDeliveryPos = 0.2;
-    public static double wristIntakePos = 0.8;
+    public static double wristIntakePos = 0.85;
     public static double droneLaunchPos = 0.5;
 
     // ------ Declare Motors ------ //
